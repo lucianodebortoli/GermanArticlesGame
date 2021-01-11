a1fec50590c9653601b813648c5f354a6d7f45dd
Luciano De Bortoli <luciano.debortoli@hipcamailab.com>
2021-01-11 02:22:34 -0300

    changed app icon

--- app/src/main/java/org/luciano/germanarticles/Entry.java
+++ app/src/main/java/org/luciano/germanarticles/Entry.java
@@ -1,36 +1,22 @@
 package org.luciano.germanarticles;
 
-
-
 public class Entry {
 
     private String ID;
     private String article;
     private String noun;
 
-    public String getID() {
-        return ID;
-    }
+    public String getID() { return ID; }
 
-    public void setID(String ID) {
-        this.ID = ID;
-    }
+    public void setID(String ID) { this.ID = ID; }
 
-    public String getArticle() {
-        return article;
-    }
+    public String getArticle() { return article; }
 
-    public void setArticle(String article) {
-        this.article = article;
-    }
+    public void setArticle(String article) { this.article = article; }
 
-    public String getNoun() {
-        return noun;
-    }
+    public String getNoun() { return noun; }
 
-    public void setNoun(String noun) {
-        this.noun = noun;
-    }
+    public void setNoun(String noun) {this.noun = noun; }
 
     @Override
     public String toString() {
